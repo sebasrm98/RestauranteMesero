ce8f0dff7f9b3158bb4c1d833530acf2d39d9c2b
sebasrm98 <79673528+sebasrm98@users.noreply.github.com>
2021-03-18 18:58:47 -0500

    versionMeseros 1.0 : interfaz mejorada

---
+++ app/src/main/java/com/example/restaurantemeseros/interfaz/PresentacionActivity.java
@@ -0,0 +1,46 @@
+package com.example.restaurantemeseros.interfaz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.example.restaurantemeseros.R;
+
+public class PresentacionActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presentacion);
+
+        progressBar=findViewById(R.id.progressBar2);
+        progressBar.setVisibility(View.VISIBLE);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences preferences= getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
+                boolean sesion=preferences.getBoolean("sesion",false);
+                if(sesion){
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        },2000);
+    }
+}
